0e16273166800f35840f53e3ab12d709e555f450
issawy <60878926+AhmedIssawy@users.noreply.github.com>
2024-10-19 23:40:41 +0300

    Add files via upload
    UI updates

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ var randomNumber2 = Math.floor(Math.random()*6+1);
 var randomPic2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomPic2);
 if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerText = "DRAW";
+    document.querySelector("h1").innerText = "DRAW!!";
 }
 else if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerText = "Player 1 WINS";
+    document.querySelector("h1").innerText = "🚩Player 1 WINS";
 }
-else{document.querySelector("h1").innerText = "Player 2 WINS";}
+else{document.querySelector("h1").innerText = "Player 2 WINS🚩";}

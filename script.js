2add308cad9b9867acf6abcb57236940ad662c16
issawy <60878926+AhmedIssawy@users.noreply.github.com>
2024-10-20 15:58:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ document.querySelector(".img1").setAttribute("src",randomPic1); //changing img s
 var randomNumber2 = Math.floor(Math.random()*6+1);
 var randomPic2 = "images/dice" + randomNumber2 + ".png";
 document.querySelector(".img2").setAttribute("src", randomPic2);
-if (randomNumber1 === randomNumber2) {
+if (randomNumber1 === randomNumber2) { // if dice1=dice2 will print DRAW!
     document.querySelector("h1").innerText = "DRAW!!";
 }
-else if(randomNumber1 > randomNumber2){
+else if(randomNumber1 > randomNumber2){ // if dice1>dice2 will print 🚩Player 1 WINS
     document.querySelector("h1").innerText = "🚩Player 1 WINS";
 }
-else{document.querySelector("h1").innerText = "Player 2 WINS🚩";}
+else{document.querySelector("h1").innerText = "Player 2 WINS🚩";} //if dice1<dice2 will print 🚩Player 1 WINS
